f5bbec5fb4dfd606bfc53d51b8d0da6f0aa908eb
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-09-15 22:48:37 -0600

    get database configuration through .env

--- config/database.conf.js
+++ config/database.conf.js
@@ -1,7 +1,7 @@
 export default {
-    host: 'db',
-    port: 3306,
-    user: 'root',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 }
